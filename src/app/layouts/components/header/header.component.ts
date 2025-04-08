fe77d2c368e6a5ca270614367f0431cb8c4d3048
sameenhewage <sameen.android@gmail.com>
2025-04-09 02:30:15 +0530

    added header ui. Updated some ui changes according to the header ui

--- src/app/layouts/components/header/header.component.ts
+++ src/app/layouts/components/header/header.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   imports: [],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
-
+  title = signal<string>('Task Manager');
 }
