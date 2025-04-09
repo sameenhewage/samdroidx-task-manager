1d51ef0f1f2bfb1fa0254bc63a912e1b2a5ca620
sameenhewage <sameen.android@gmail.com>
2025-04-09 09:56:00 +0530

    feat(sidebar): add sidebar navigation component
    - Created SidebarNavComponent
    - Added unit test for SidebarNavComponent
    - Integrated SidebarNavComponent into SidebarComponent

---
+++ src/app/core/shared/sidebar-nav/sidebar-nav.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sidebar-nav',
+  imports: [],
+  templateUrl: './sidebar-nav.component.html',
+  styleUrl: './sidebar-nav.component.scss'
+})
+export class SidebarNavComponent {
+
+}
--- src/app/layouts/components/sidebar/sidebar.component.ts
+++ src/app/layouts/components/sidebar/sidebar.component.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RoleTitleComponent } from '../../../core/shared/role-title/role-title.component';
+import { SidebarNavComponent } from '../../../core/shared/sidebar-nav/sidebar-nav.component';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [RoleTitleComponent],
+  imports: [RoleTitleComponent, SidebarNavComponent],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss',
 })
