273b5dc8ffc12da799ce8c329a6de02c8ecf7d75
sameenhewage <sameen.android@gmail.com>
2025-04-10 08:27:54 +0530

    feat(sidebar): optimize profile picture loading
    - Added `ngSrc` directive for lazy loading
    - Set image dimensions for better performance
    - Updated SCSS to remove redundant styles
    - Imported `NgOptimizedImage` module

--- src/app/layouts/components/sidebar/sidebar.component.ts
+++ src/app/layouts/components/sidebar/sidebar.component.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RoleTitleComponent } from '../../../core/shared/role-title/role-title.component';
 import { SidebarNavComponent } from '../../../core/shared/sidebar-nav/sidebar-nav.component';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [RoleTitleComponent, SidebarNavComponent],
+  imports: [NgOptimizedImage, RoleTitleComponent, SidebarNavComponent],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss',
 })
