b98c02f42a443b90bf79056f3386c2ef25889577
sameenhewage <sameen.android@gmail.com>
2025-04-11 21:40:50 +0530

    test(layouts): update main component tests
    - Add animations providers
    - Update test configuration

--- src/app/layouts/main/main.component.spec.ts
+++ src/app/layouts/main/main.component.spec.ts
@@ -3,6 +3,8 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MainComponent } from './main.component';
 import { DashboardComponent } from '../../features/components/dashboard/dashboard.component';
 import { provideRouter } from '@angular/router';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 describe('MainComponent', () => {
   let component: MainComponent;
@@ -11,7 +13,10 @@ describe('MainComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [MainComponent],
-      providers: [provideRouter([{ path: '', component: DashboardComponent }])],
+      providers: [
+        provideRouter([{ path: '', component: DashboardComponent }]),
+        provideAnimationsAsync(),
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(MainComponent);
